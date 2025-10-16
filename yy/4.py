3d30f2f4bb4510aa009796dd94b56b302ac0f1f3
butterfly-yy <butterfly-baicai@outlook.com>
2025-10-16 23:13:15 +0800

    2025.10.1

---
+++ yy/4.py
@@ -0,0 +1,85 @@
+'''
+给你一个整数数组 nums ，请你找出一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+子数组是数组中的一个连续部分。
+
+示例 1：
+输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+输出：6
+解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+
+示例 2：
+输入：nums = [1]
+输出：1
+
+示例 3：
+输入：nums = [5,4,-1,7,8]
+输出：23
+'''
+class Solution:
+    #动态规划 时间复杂度O(n) 空间复杂度O(n)
+    #状态转移方程：dp[i]=max(dp[i-1]+nums[i],nums[i])
+    def maxSubArray(self, nums: List[int]) -> int:
+        size=len(nums)
+        if size==0:
+            return 0
+        dp=[0 for _ in range(size)]
+
+        dp[0]=nums[0]
+        for i in range(1,size):
+            if dp[i-1]>=0:
+                dp[i]=dp[i-1]+nums[i]
+            else:
+                dp[i]=nums[i]
+        return max(dp)
+    
+    from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        size = len(nums)
+        if size == 0:
+            return 0
+        return self.__max_sub_array(nums, 0, size - 1)
+
+    def __max_sub_array(self, nums, left, right):
+        if left == right:
+            return nums[left]
+        mid = (left + right) >> 1
+        return max(self.__max_sub_array(nums, left, mid),
+                   self.__max_sub_array(nums, mid + 1, right),
+                   self.__max_cross_array(nums, left, mid, right))
+
+
+    #分治法 时间复杂度O(nlogn) 空间复杂度O(logn)
+    def a(self, nums, left, mid, right):
+        # 一定包含 nums[mid] 元素的最大连续子数组的和，
+        # 思路是看看左边"扩散到底"，得到一个最大数，右边"扩散到底"得到一个最大数
+        # 然后再加上中间数
+        left_sum_max = 0
+        start_left = mid - 1
+        s1 = 0
+        while start_left >= left:
+            s1 += nums[start_left]
+            left_sum_max = max(left_sum_max, s1)
+            start_left -= 1
+
+        right_sum_max = 0
+        start_right = mid + 1
+        s2 = 0
+        while start_right <= right:
+            s2 += nums[start_right]
+            right_sum_max = max(right_sum_max, s2)
+            start_right += 1
+        return left_sum_max + nums[mid] + right_sum_max
+    
+
+    #前缀和+贪心算法 时间复杂度O(n) 空间复杂度O(1)
+    def b(self, nums: List[int]) -> int:
+        ans = -inf
+        min_pre_sum = pre_sum = 0
+        for x in nums:
+            pre_sum += x  # 当前的前缀和
+            ans = max(ans, pre_sum - min_pre_sum)  # 减去前缀和的最小值
+            min_pre_sum = min(min_pre_sum, pre_sum)  # 维护前缀和的最小值
+        return ans
