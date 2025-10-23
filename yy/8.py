a5495182ed7a387625fdd53829a33e2b34ddeaba
butterfly-yy <butterfly-baicai@outlook.com>
2025-10-23 14:47:10 +0800

    2025.10.23

--- yy/8.py
+++ yy/8.py
@@ -1,3 +1,4 @@
+'''
 给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
 
 示例 1：
@@ -11,7 +12,7 @@
 示例 3：
 输入：head = [1]
 输出：[1]
-
+'''
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
