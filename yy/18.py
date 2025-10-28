7cf2bc37bf5fa6cd3de96056a2b70b3f28e7b5bb
butterfly-yy <butterfly-baicai@outlook.com>
2025-10-28 23:23:27 +0800

    2025.10.28

---
+++ yy/18.py
@@ -0,0 +1,47 @@
+'''给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。答案可以按 任意顺序 返回。
+给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。
+
+示例 1：
+输入：digits = "23"
+输出：["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+示例 2：
+输入：digits = "2"
+输出：["a","b","c"]'''
+
+class Solution:
+    #回溯，每次从digits中取出一个数字，将其对应的字母添加到当前组合中，递归到下一个数字
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+
+        phone = {'2':['a','b','c'],
+                 '3':['d','e','f'],
+                 '4':['g','h','i'],
+                 '5':['j','k','l'],
+                 '6':['m','n','o'],
+                 '7':['p','q','r','s'],
+                 '8':['t','u','v'],
+                 '9':['w','x','y','z']}
+                
+        def backtrack(conbination,nextdigit):
+            if len(nextdigit) == 0:
+                res.append(conbination)
+            else:
+                for letter in phone[nextdigit[0]]:
+                    backtrack(conbination + letter,nextdigit[1:])
+
+        res = []
+        backtrack('',digits)
+        return res
+
+    #队列，每次弹出一个元素，将其后面的字母添加到队列中，直到队列中所有元素都被处理完，返回队列中的元素
+    def a(self, digits: str) -> List[str]:
+        if not digits: return []
+        phone = ['abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
+        queue = ['']  # 初始化队列
+        for digit in digits:
+            for _ in range(len(queue)):
+                tmp = queue.pop(0)
+                for letter in phone[ord(digit)-50]:# 这里我们不使用 int() 转换字符串，使用ASCII码
+                    queue.append(tmp + letter)
+        return queue
