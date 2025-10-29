31d597a7f2ff0c6b3ac45f35f0197b73782c3b97
butterfly-yy <butterfly-baicai@outlook.com>
2025-10-29 20:14:24 +0800

    2025.10.29

---
+++ yy/20.py
@@ -0,0 +1,27 @@
+'''给定两个整数数组 preorder 和 inorder ，其中 preorder 是二叉树的先序遍历， inorder 是同一棵树的中序遍历，请构造二叉树并返回其根节点。
+
+示例 1:
+输入: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+输出: [3,9,20,null,null,15,7]
+
+示例 2:
+输入: preorder = [-1], inorder = [-1]
+输出: [-1]'''
+
+#递归
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if len(preorder)==0:#两个列表都可以
+            return None
+        root=TreeNode(preorder[0])
+        mid=inorder.index(preorder[0])#这里的mid既是下标，mid+1也是左子树的长度
+        #构造左子树
+        root.left=self.buildTree(preorder[1:mid+1],inorder[:mid])
+        root.right=self.buildTree(preorder[mid+1:],inorder[mid+1:])
+        return root
